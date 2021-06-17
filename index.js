5527bffade1eecfdb22f9e33d3109457214a11f3
Falador <mrfalador@gmail.com>
2021-06-17 20:51:53 +0300

    I forget commit this file

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-const express = require('express')
-const app = express()
+const path = require('path');
+const express = require('express');
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => {
-    res.send({message: 'Example'});
+    res.sendFile('${__dirname}/public/index.html');
 });
 
 app.listen(3000, () => {
     console.log('Application listening on port 3000');
 });
+
