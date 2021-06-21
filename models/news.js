8400a1a8725e24ad9f966d53c4b53657c5b91cf7
Falador <mrfalador@gmail.com>
2021-06-21 19:26:36 +0300

    Model Quotes

--- models/news.js
+++ models/news.js
@@ -5,38 +5,38 @@ var Schema = mongoose.Schema;
 var NewsSchema = new Schema(
     {
         "properties":{
-        "item":{
-        "items":{
-        "properties":{
-        "description":{
-        "type":"string"
-        },
-        "guid":{
-        "type":"string"
-        },
-        "link":{
-        "type":"string"
-        },
-        "pubDate":{
-        "type":"string"
-        },
-        "title":{
-        "type":"string"
-        }
+            "item":{
+                "items":{
+                    "properties":{
+                        "description":{
+                            "type":"string"
+                        },
+                        "guid":{
+                            "type":"string"
+                        },
+                        "link":{
+                            "type":"string"
+                        },
+                        "pubDate":{
+                            "type":"string"
+                        },
+                        "title":{
+                            "type":"string"
+                        }
+                    },
+                    "type":"object"
+                },
+                "type":"array"
+            },
+            "language":{
+            "type":"string"
+            },
+            "lastBuildDate":{
+            "type":"string"
+            }
         },
         "type":"object"
-        },
-        "type":"array"
-        },
-        "language":{
-        "type":"string"
-        },
-        "lastBuildDate":{
-        "type":"string"
-        }
-        },
-        "type":"object"
-        }
+    }
 )
 
-module.exports = mongoose.model('News', NewsSchema);
+module.exports = mongoose.model('Quotes', NewsSchema);
--- models/quotes.js
+++ models/quotes.js
@@ -1,7 +1,42 @@
+
 var mongoose = require('mongoose');
 
 var Schema = mongoose.Schema;
 
 var QuotesSchema = new Schema(
-    
+    {
+        "items":{
+            "properties":{
+                "ask":{
+                    "type":"integer"
+                },
+                "askSize":{
+                    "type":"integer"
+                },
+                "bid":{
+                    "type":"integer"
+                    },
+                "bidSize":{
+                    "type":"integer"
+                },
+                "bookValue":{
+                    "type":"number"
+                },
+                "language":{
+                    "type":"string"
+                },
+                "longName":{
+                    "type":"string"
+                },
+                "market":{
+                    "type":"string"
+                },
+                "marketCap":{
+                    "type":"integer"
+                },
+            }
+            "type":"object"
+        }
+        "type":"array"
+        }
 )
